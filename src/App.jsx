7241bd3f836b4d3fd1618e69275ed834ae1d00e0
partik0607 <partiksehrawat06@gmail.com>
2025-02-19 14:50:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './index.css';
+
+function App() {
+    const [name, setName] = useState('');
+    const [greeting, setGreeting] = useState('');
+    
+    const fetchGreeting = async () => {
+        if (!name) {
+            setGreeting('Name is required.');
+            return;
+        }
+        try {
+            const response = await fetch(`http://greating-app-backend.onrender.com/api/greet?name=${name}`);
+            const data = await response.json();
+            setGreeting(data.message || data.error);
+        } catch (error) {
+          console.log(error);
+            setGreeting('Something wents wrong');
+        }
+    };
+
+    return (
+        <div className="container">
+            <h1>Greeting App</h1>
+            <input type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} />
+            <button onClick={fetchGreeting}>Get Greeting</button>
+            <h3>{greeting}</h3>
+        </div>
+    );
+}
+
+export default App;
